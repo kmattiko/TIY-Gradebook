8f4ecfb6656ad9c0c83b77ad47db6b6dc99c14b9
Kim Mattiko <amberlie@rocketmail.com>
2015-06-24 15:10:05 -0400

    making changes to index.html and main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,16 @@
-console.log('\'Allo \'Allo!');
+(function(window){
+
+var app = angular.model(‘tiy-gradebook’, [ ]);
+
+app.controller(‘MainController’, function(){
+     this.page = function(name){
+     if ( name == “milestones’ ){
+          this.view = ‘views/milestones.html’;
+     }
+     if ( name == ‘repositories’ ){
+          this.view = ‘views/repositories.html’;
+     }
+
+this.page(‘repositories);
+
+})(window);
