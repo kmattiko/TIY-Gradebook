235ab2c4ee7051f250f159ec532e933a0841c3a8
Kim Mattiko <amberlie@rocketmail.com>
2015-06-25 14:20:28 -0400

    pushing some code from thursday lecture

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,8 +2,9 @@
 
 var app = angular.model('tiy-gradebook', [ ]);
 
-app.controller('MainController', function(){
+app.controller('MainController', function($http){
      this.page = function(name){
+
      if ( name == 'milestones'){
           this.view = '/views/milestones.html';
      }
@@ -15,5 +16,53 @@ app.controller('MainController', function(){
      }
 
 this.page('repositories');
+
+this.repos = [ ];
+
+/*var self = this, // keep a reference to 'this' for late
+    //that = this, // PLEASE DON"T DO THIS
+    app = this; //Just a convienence*/
+
+$http.get('/api/github/orgs/TheIronYard--Orlando/repos.json')
+  .then(function(response){
+    self.repos = response.data;
+
+    /*  return !( repo.name.indexOf('FEE') === -1 );
+    });
+  }, function(){
+    console.log('WHY?');
+  }*/
+
+  function apiUrl (path){
+    return '/api/github/TheIronYard--Orlando/2015--SUMMER--FEE/' + path;
+  }
+
+  app.controller('CohortController', function($http){
+    var self = this;
+    self.assignments = [ ];
+
+
+      $http.get('/api/github/milestones.json')
+      .then(function(response){
+        self.assignments = response.data
+      })
+//var LinkHeader = xhr.getAllResponseHeaders()('Link')
+      for ( var index = 0; index < response.data.length; index++ ) {
+        var milestone = response.data[index];
+        $http.get(apiUrl('milestone/' + milestone.number + '/issues.json'))
+      }
+
+  });
+});
+    respone.data.forEach(function(item, index, all){
+      var milestone = response.data[index];
+
+      $http.get(apiUrl('milestone/' + milestone.number + '/issues.json'));
+    });
+
+    _.forEach()
+
+  });
+});
 
 })(window);
