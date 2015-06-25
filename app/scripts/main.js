79909b90e89f22e79fe1362be51caf760b9ace53
Kim Mattiko <amberlie@rocketmail.com>
2015-06-25 08:58:43 -0400

    updating

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,19 @@
 (function(window){
 
-var app = angular.model(‘tiy-gradebook’, [ ]);
+var app = angular.model('tiy-gradebook', [ ]);
 
-app.controller(‘MainController’, function(){
+app.controller('MainController', function(){
      this.page = function(name){
-     if ( name == “milestones’ ){
-          this.view = ‘views/milestones.html’;
+     if ( name == 'milestones'){
+          this.view = '/views/milestones.html';
      }
-     if ( name == ‘repositories’ ){
-          this.view = ‘views/repositories.html’;
+     if ( name == 'repositories' ){
+          this.view = '/views/repositories.html';
+     }
+     else {
+          this.view == '404.html';
      }
 
-this.page(‘repositories);
+this.page('repositories');
 
 })(window);
